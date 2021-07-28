c85b2ba01574b52fd350626fb642b4d714838c7f
worldnas <ar.jafari83@gmail.com>
2021-07-28 08:08:11 -0800

    easy

---
+++ Untitled-3.js
@@ -0,0 +1,62 @@
+//and 
+
+//1&1=>1
+//1&0=>0
+//0&0=>0
+//1=true
+//0=false
+console.log(1&0)
+
+
+//or
+
+//1|1=>1
+//1|0=>1
+//0|0=>0
+console.log(1|0)
+
+
+//xor
+
+//0^0=>0
+//1^0=>1
+//1^1=>0
+console.log(1^1)
+
+
+//not
+
+
+//~1=>-2
+//~0=>-1
+console.log(~1)
+
+
+//SHIFT RIGHT
+
+//0>>1=>0
+//1>>0=>1
+console.log(104>>2)
+
+
+//SHIFT LEFT
+
+//0<<1=>0
+//1<<0=>1
+console.log(8<<2)
+
+
+
+//ANOTHER PRACTICE
+
+
+var num1=20
+var num2=40
+if(num1<=20 & num2>=50)
+{
+	console.log("yes")
+}
+else
+{
+	console.log('no')
+}
